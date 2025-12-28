44ef164714904c5c811824ffee9a9a9619df6fe4
Andrea Amutio <andiamutio93@gmail.com>
2025-12-27 23:18:07 -0300

    conectar componentes a App

--- todo-list-react/src/App.jsx
+++ todo-list-react/src/App.jsx
@@ -1,3 +1,7 @@
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
+
 function App() {
   return (
     <section className="section">
@@ -5,6 +9,9 @@ function App() {
         <h1 className="title has-text-centered has-text-info">
           Todo List React
         </h1>
+
+        <Form />
+        <TodoList />
       </div>
     </section>
   );
