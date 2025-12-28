6127cae37e3740ab5b95803f35cab84a8bd3b264
Andrea Amutio <andiamutio93@gmail.com>
2025-12-28 04:02:39 -0300

    Estado app.jsx listo

--- todo-list-react/src/App.jsx
+++ todo-list-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
+import './index.css';
 
 function App() {
   const [tareas, setTareas] = useState([]);
@@ -23,19 +24,16 @@ function App() {
     <section className="section">
       <div className="container is-max-widescreen">
         <div
-          className="box has-background-grey-dark"
+          className="box has-background-dark"
           style={{
-            borderRadius: "20px",
-            padding: "3rem",
-            boxShadow: "0 20px 40px rgba(0,0,0,0.6)",
+           borderRadius: "100px 100px",
+           padding: "3rem",
+           boxShadow: "0 0px 50px rgba(4, 222, 255, 0.7)",
+           minHeight: "80vh",
           }}
         >
-          <h1 className="title has-text-centered has-text-info-light">
-            📋 Mis Tareas
-          </h1>
-
+          <h1 className="title has-text-centered has-text-info-light"> Tareas del día 📋 </h1>
           <Form tareas={tareas} setTareas={setTareas} />
-
           <TodoList
             tareas={tareas}
             setTareas={setTareas}
